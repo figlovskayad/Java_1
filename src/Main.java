437325049ed4777674d39a87c8f5e26d375949a7
netol <figlovskayad@gmail.com>
2023-03-23 15:59:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15780;
+        int bonus = 20;
+        {
+            System.out.println("Количество бонусов " + amount / bonus);
+        }
+    }
+}
